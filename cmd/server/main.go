1f7e5038739b0cb87942a749f944aa971cedc8bd
Nathan Fabio <nathanfabio.dev@gmail.com>
2025-04-15 20:52:46 -0300

    Implement task management routes with Create and List functionalities

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,16 +5,26 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/nathanfabio/level1-todoApi/internal/handler"
+	"github.com/nathanfabio/level1-todoApi/internal/repository"
+	"github.com/nathanfabio/level1-todoApi/pkg/db"
 )
 
 
 func main() {
+	database := db.Connect()
+	repo := repository.NewTaskRepository(database)
+	taskHandler := handler.NewTaskHandler(repo)
+
 	r := chi.NewRouter()
 
 	//Test
 	r.Get("/ping", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("pong"))
 	})
+
+	r.Post("/tasks", taskHandler.CreateTask)
+	r.Get("/tasks", taskHandler.ListTasks)
 
 
 	log.Println("Starting server on :7070")

c78077bbffaa5acbf2619c801c4b8e2d260564f5
Nathan Fabio <nathanfabio.dev@gmail.com>
2025-04-15 20:52:33 -0300

    Add TaskRepository implementation for task management

---
+++ internal/repository/task_repository.go
@@ -0,0 +1,25 @@
+package repository
+
+import (
+	"github.com/jmoiron/sqlx"
+	"github.com/nathanfabio/level1-todoApi/internal/model"
+)
+
+type TaskRepository struct {
+	db *sqlx.DB
+}
+
+func NewTaskRepository(db *sqlx.DB) *TaskRepository {
+	return &TaskRepository{db: db}
+}
+
+func (r *TaskRepository) Create(task *model.Task) error {
+	_, err := r.db.NamedExec(`INSERT INTO tasks (title, done) VALUES (:title, :done)`, task)
+	return err
+}
+
+func (r *TaskRepository) GetAll() ([]model.Task, error) {
+	var tasks []model.Task
+	err := r.db.Select(&tasks, "SELECT * FROM tasks ORDER BY created_at DESC")
+	return tasks, err
+}
